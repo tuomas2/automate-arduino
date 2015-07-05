902e426c1d44fcc496fdd84ef6b16d512aaa03b1
Tuomas Airaksinen <tuomas.airaksinen@gmail.com>
2015-07-05 12:33:15 +0300

    Fix setup.py and requirements.pip for the first release

--- automate_arduino/__init__.py
+++ automate_arduino/__init__.py
@@ -16,6 +16,15 @@
 # You should have received a copy of the GNU General Public License
 # along with automate_arduino.  If not, see <http://www.gnu.org/licenses/>.
 
+__author__ = "Tuomas Airaksinen"
+__copyright__ = "Copyright 2015, Tuomas Airaksinen"
+__credits__ = []
+__license__ = "GPL"
+__version__ = '0.9'
+__maintainer__ = "Tuomas Airaksinen"
+__email__ = "tuomas.airaksinen@gmail.com"
+__status__ = "Beta"
+
 from .arduino_service import ArduinoService
 from .arduino_actuators import ArduinoDigitalActuator, ArduinoPWMActuator, ArduinoServoActuator, AbstractArduinoActuator
 from .arduino_sensors import ArduinoAnalogSensor, ArduinoDigitalSensor, AbstractArduinoSensor
